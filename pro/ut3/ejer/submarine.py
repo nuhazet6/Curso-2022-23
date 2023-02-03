cfc317e80daab5cd15f53ff9f685be9bf753d253
nuhazet6 <nuhazet6@gmail.com>
2023-02-03 05:25:10 +0000

    Se termina el ejercicio submarine.py, aún falta refactorizar

--- pro/ut3/ejer/submarine.py
+++ pro/ut3/ejer/submarine.py
@@ -9,10 +9,15 @@ def run(route_path: Path) -> tuple:
     with open(route_path, "r") as f:
         fuel = int(f.readline().strip())
         movements = f.readline().split(",")
+        distance = 0
+        depth = 0
         for movement in movements:
             dist_depth = movement.split(":")
-
-    distance = depth = fuel = "output"
+            distance += int(dist_depth[0])
+            depth += int(dist_depth[1])
+            fuel -= abs(int(dist_depth[0]) * 3)
+            if fuel <= 0 or 600 < depth or depth < 0:
+                break
 
     return distance, depth, fuel
 
