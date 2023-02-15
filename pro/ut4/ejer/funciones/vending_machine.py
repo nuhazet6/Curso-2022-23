ebd73b7a638ac1297f2a5ac76de6123167c670be
nuhazet6 <nuhazet6@gmail.com>
2023-02-15 20:20:16 +0000

    texto

--- pro/ut4/ejer/funciones/vending_machine.py
+++ pro/ut4/ejer/funciones/vending_machine.py
@@ -1,20 +1,34 @@
+with open("envios.dat", "r") as f:
+    clients = []
+    codes = set()
+    for line, client in enumerate(f):
+        info_envio = line.strip().split()
+        code = info_envio[0]
+        amount = int(info_envio[1])
+        money = int(info_envio[2])
+        clients.append((code, amount, money))
 with open("stock.dat", "r") as f:
-    product = {}
+    products = {}
     for line in f:
         info_product = line.strip().split()
         code = info_product[0]
         stock = int(info_product[1])
         price = int(info_product[2])
-        product[code] = {"stock": stock, "price": price}
-print(product)
+        products[code] = {"stock": stock, "price": price}
+print(products)
 
 
-def price(*, code=""):
-    info = products.get(code, 0)
-    if info is None:
-        return None
-    return info.values()[0]
+def price(*, code):
+    info = products.get(code, -1)
+    price = info.values()[0]
+    return price
 
 
-def have_stock(*, code=""):
-    info = products.get(code, 0)
+def stock(*, code):
+    info = products.get(code, -2)
+    stock = info.values()[1]
+    return stock
+
+def buy(*,code,amount,money):
+    total_price = price(code)*
+    if price(code = code)
