c00287817a038de7b92894ffb779525d1b43f491
nuhazet6 <nuhazet6@gmail.com>
2023-04-18 09:51:13 +0100

    Se terminan las funcionalidades de la clase date, pendiente de la revisión en clase.

--- PRO/ut4/ejer/Objetos_y_Clases/date.py
+++ PRO/ut4/ejer/Objetos_y_Clases/date.py
@@ -1,7 +1,8 @@
-FIRST_YEAR = 1900
-LAST_YEAR = 2050
-
-
+FIRST_YEAR_VALID = 1900
+LAST_YEAR_VALID = 2050
+WEEKDAYS = ['domingo','lunes','martes','miércoles','jueves','viernes','sábado']
+WEEKEND = [WEEKDAYS.index('sábado'),WEEKDAYS.index('domingo')]
+MONTHS = {1:'enero',2:'febrero',3:'marzo',4:'abril',5:'mayo',6:'junio',7:'julio',8:'agosto',9:'septiembre',10:'octubre',11:'noviembre',12:'diciembre'}
 class Date:
     def __init__(self, day: int, month: int, year: int):
         """Validar día, mes y año. Se comprobará si la fecha es correcta
@@ -9,67 +10,169 @@ class Date:
         si el mes no es correcto, lo pondrá a 1; y si el año no es correcto, lo pondrá a 1900.
         Ojo con los años bisiestos.
         """
-        self.day = day
-        self.month = month
-        self.year = year
-        if FIRST_YEAR > year > LAST_YEAR:
-            self.year = FIRST_YEAR
-        if 0 > self.month > 12:
-            self.month = 1
+        self.year = year if FIRST_YEAR_VALID < year < LAST_YEAR_VALID else FIRST_YEAR_VALID
+        self.month = month if month in MONTHS else 1
+        self.day = day if self.days_in_month(self.year,self.month) >= day > 0 else 1 
 
     @staticmethod
-    def is_leap_year(year) -> bool:
+    def is_leap_year(year:int) -> bool:
         return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
 
-    @property
-    def days_in_month(self) -> int:
-        if self.month == 2:
-            return 28 + self.is_leap_year(self.year)
+    @staticmethod
+    def days_in_month(year:int, month:int) -> int:
+        if month == 2:
+            return 28 + Date.is_leap_year(year)
         DAYS_IN_MONTH_ODD_EVEN = [30, 31]
-        is_month_second_part = self.month > 7
+        is_month_second_part = month > 7
         if is_month_second_part:
-            return DAYS_IN_MONTH_ODD_EVEN[(self.month - 7) % 2]
-        return DAYS_IN_MONTH_ODD_EVEN[self.month % 2]
+            return DAYS_IN_MONTH_ODD_EVEN[(month - 7) % 2]
+        return DAYS_IN_MONTH_ODD_EVEN[month % 2]
 
     def delta_days(self) -> int:
         """Número de días transcurridos desde el 1-1-1900 hasta la fecha"""
         total_days = 0
-        for i in range(FIRST_YEAR, self.year):
+        for i in range(FIRST_YEAR_VALID, self.year):
             if self.is_leap_year(i):
                 total_days += 366
             else:
                 total_days += 365
-
+        for i in range(1,self.month):
+            total_days += self.days_in_month(self.year,i)
+        total_days += self.day -1 #Se tiene que restar 1 porque si no se contaría a sí mismo y tendríamos un día de más
         return total_days
 
     def weekday(self) -> int:
         """día de la semana de la fecha (0 para domingo, ..., 6 para sábado).
-        El 1-1-1900 fue domingo."""
-        pass
+        El 1-1-1900 fue lunes."""
+        return self.delta_days()%7
 
     def is_weekend(self) -> bool:
-        pass
+        return self.weekday() in WEEKEND
 
     def short_date(self) -> str:
         """02/09/2003"""
-        pass
+        return f'{self.day:02d}/{self.month:02d}/{self.year}'
 
     def __str__(self):
         """martes 2 de septiembre de 2003"""
-        pass
+        weekday = WEEKDAYS[self.weekday()]
+        month = MONTHS[self.month]
+        return f'{weekday} {self.day} de {month} de {self.year}'
+
+    def __add__(self,days: int):
+        ''' '''
+        new_year = self.year
+        new_month = self.month
+        new_day = self.day  
+
+        # leap_year = self.year
+        # if self.month > 2:
+        #     leap_year += 1
+        # if self.is_leap_year(leap_year):
+        #     days -= 1
+        #     if days < 365:
+        #         new_day += 1
+        # while leap_year < (self.year + 9) or days >= 365:
+        #     if self.is_leap_year(leap_year):
+        #         break
+        #     days-= 365
+        #     new_year += 1
+        #     leap_year += 1
+        # years_to_add,days = divmod(days,365)
+        # days -= years_to_add // 4
+        # new_year += years_to_add
+        
+        if self.month > 2:
+            target_year = new_year + 1
+        else:
+            target_year = new_year
+        while days >= 365 + self.is_leap_year(target_year):
+            days_in_current_year = 365 + self.is_leap_year(target_year)
+            days -= days_in_current_year
+            new_year += 1
+            target_year += 1          
+
+        while days > (self.days_in_month(new_year,new_month) - new_day):
+            days -= self.days_in_month(new_year,new_month)
+            new_month += 1
+            if new_month == 13:
+                new_month = 1
+                new_year += 1
+        new_day += days
+        return Date(new_day,new_month,new_year)
 
     def __sub__(self, other):
-        if self.year > other.year:
-            lower = other
-            upper = self
-        else:
-            lower = self
-            upper = other
-        new_year = upper.year - lower.year
-        new_month = 12 - lower.month + upper.month
-        new_days = lower.days_in_month() - lower.days + upper.days_in_month
-        return
+        if isinstance(other,Date):
+            return abs(self.delta_days() - other.delta_days())
+        if isinstance(other,int): #ó else
+            new_year = self.year
+            if self.month <= 2:
+                target_year = new_year - 1
+            else:
+                target_year = new_year
+            while other >= 365 + self.is_leap_year(target_year):
+                other -= 365 + self.is_leap_year(target_year)
+                new_year -= 1
+                target_year -= 1
+            
+            new_month = self.month
+            target_month = new_month - 1
+            target_year = new_year
+            if target_month <= 0:
+                target_month = 12
+                target_year = new_year -1
+            while other >= self.days_in_month(target_year,target_month):
+                other -= self.days_in_month(target_year,target_month)
+                new_month -= 1
+                target_month -= 1
+                if new_month <= 0:
+                    new_month = 12
+                    new_year -= 1
+                if target_month <= 0:
+                    target_month = 12
+                    target_year -=1
 
+            new_day = self.day - other
+            if new_day <= 0:
+                new_day = self.days_in_month(target_year,target_month) + new_day 
+                new_month = target_month
+                if new_month == 12:
+                    new_year = target_year
+
+        return Date(new_day,new_month,new_year)
+
+    def __eq__(self, other) -> bool:
+        are_years_eq = self.year == other.year
+        are_months_eq = self.month == other.month
+        are_days_eq = self.day == other.day
+        return all([are_years_eq,are_months_eq,are_days_eq])
+    
+    def __gt__(self,other) -> bool:
+        if self.year > other.year:
+            return True
+        if self. year == other.year:
+            if self.month > other.month:
+                return True
+            if self.month == other.month:
+                if self.day > other.day:
+                    return True
+        return False
+    
+    def __ge__(self,other) -> bool:
+        if self > other or self == other:
+            return True
+        return False
+
+    def __lt__(self,other) -> bool:
+        if self >= other:
+            return False
+        return True
+    
+    def __le__(self,other) -> bool:
+        if self < other or self == other:
+            return True
+        return False
+    
     # operador + suma días a la fecha
     # operador - resta días a la fecha o calcula la diferencia entre dos fechas
     # operador == dice si dos fechas son iguales
@@ -77,7 +180,20 @@ class Date:
     # operador < dice si una fecha es menor que otra
 
 
-fecha1 = Date(1, 1, 1901)
-fecha2 = Date(30, 2, 2020)
+fecha1 = Date(1, 1, 1905)
+print(fecha1)
+fecha2 = Date(31, 2, 1900)
+print(fecha2)
+fecha4 = Date(30, 1, 1901)
 print(Date.is_leap_year(fecha1.year), Date.is_leap_year(fecha2.year))
-print(fecha1.delta_days())
+print(fecha1.delta_days(),fecha2.delta_days())
+print(fecha1 - fecha2)
+fecha3 = fecha1 + 366
+print(fecha3)
+print(fecha1.weekday())
+print(fecha1 - 367)
+print(fecha2 > fecha4)
+print(fecha2 >= fecha4)
+print(fecha2 < fecha4)
+print(fecha2 <= fecha4)
+print(fecha1.short_date())
