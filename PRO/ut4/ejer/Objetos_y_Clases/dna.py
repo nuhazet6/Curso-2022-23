3cd01f05b18e28fea02f4356dbc819f0d047901c
nuhazet6 <nuhazet6@gmail.com>
2023-04-27 20:08:53 +0100

    Se ajusta la clase dna a los tests correspondientes, pendiente el método getter

--- PRO/ut4/ejer/Objetos_y_Clases/dna.py
+++ PRO/ut4/ejer/Objetos_y_Clases/dna.py
@@ -1,38 +1,103 @@
 class DNA:
+    VALID_BASES = "ATCG"
     ADENINE = "A"
     THYMINE = "T"
     CYTOSINE = "C"
     GUANINE = "G"
 
-    def __init__(self, dna_sequence: str):
-        self.dna_sequence = dna_sequence.upper()
+    @property
+    def adenines(self):
+        return self.sequence.count(DNA.ADENINE)
+
+    @property
+    def thymines(self):
+        return self.sequence.count(DNA.THYMINE)
+
+    @property
+    def cytosines(self):
+        return self.sequence.count(DNA.CYTOSINE)
+
+    @property
+    def guanines(self):
+        return self.sequence.count(DNA.GUANINE)
+
+    def __init__(self, sequence: str):
+        self.sequence = sequence.upper()
 
     def __str__(self) -> str:
-        return self.dna_sequence
+        return self.sequence
 
-    @property
-    def count_adenine(self):
-        return self.dna_sequence.count(DNA.ADENINE)
+    def __len__(self):
+        return len(self.sequence)
 
-    @property
-    def count_thymine(self):
-        return self.dna_sequence.count(DNA.THYMINE)
+    def __add__(self, other):
+        new_sequence = "".join(
+            [max(base1, base2) for base1, base2 in zip(self.sequence, other.sequence)]
+        )
+        remaining_bases = (
+            self.sequence[len(other) - len(self) :]
+            if len(self) > len(other)
+            else (
+                other.sequence[len(self) - len(other) :]
+                if len(self) < len(other)
+                else ""
+            )
+        )
 
-    @property
-    def count_cytosine(self):
-        return self.dna_sequence.count(DNA.CYTOSINE)
+        return DNA(new_sequence + remaining_bases)
 
-    @property
-    def count_guanine(self):
-        return self.dna_sequence.count(DNA.GUANINE)
+    def __mul__(self, other):
+        return DNA(
+            "".join(
+                [
+                    base1
+                    for base1, base2 in zip(self.sequence, other.sequence)
+                    if base1 == base2
+                ]
+            )
+        )
 
-    def __add__(self):
+    def __setitem__(self, index, base):
+        result = list(self.sequence)
+        result[index] = base if base in DNA.VALID_BASES else "A"
+        self.sequence = "".join(result)
 
-dna_seq1 = DNA("AAAGTCTTGAC")
+    @classmethod
+    def build_from_file(cls, path):
+        # metodo de clase para construir desde fichero
+        with open(path, "r") as f:
+            file_text = f.read().strip().replace("\n", "")
+        return DNA(file_text)
+
+    def dump_to_file(self, path) -> None:
+        # metodo de instancia para volcar la información a un fichero
+        with open(path, "w") as f:
+            file_text = f.write(self.sequence)
+
+    def stats(self) -> dict:
+        all_bases = len(self)
+        return dict(
+            A=round(self.adenines / all_bases * 100, 3),
+            C=round(self.cytosines / all_bases * 100, 3),
+            G=round(self.guanines / all_bases * 100, 3),
+            T=round(self.thymines / all_bases * 100, 3),
+        )
+
+    # Implementar metodo para cambiar la base en x posición
+
+
+print(DNA.ADENINE)
+dna_seq1 = DNA("ATGGT")
 print(dna_seq1)
-print(dna_seq1.count_adenine)
-print(dna_seq1.count_cytosine)
-print(dna_seq1.count_guanine)
-print(dna_seq1.count_thymine)
-dna_seq2 = DNA("GAAGTCTTGAT")
-
+print(dna_seq1.adenines)
+print(dna_seq1.cytosines)
+print(dna_seq1.guanines)
+print(dna_seq1.thymines)
+dna_seq2 = DNA("TAAGT")
+print(dna_seq1 + dna_seq2)
+print(dna_seq1.stats())
+print(dna_seq1 * dna_seq2)
+dna_seq1[4] = "G"
+print(dna_seq1)
+dna_seq1[4] = "P"
+print(dna_seq1)
